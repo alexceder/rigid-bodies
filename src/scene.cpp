ae6679a9392e5666ca2ba31b0cbcab58ef8450ea
Philburridge <burridge@live.se>
2014-01-31 13:29:26 +0100

    Added som shell functions

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,1 +1,38 @@
 #include "precompiled.h"
+
+
+void Scene::checkCollisions()  
+{
+
+};
+
+void Scene::applyImpulse();
+{
+    
+};
+
+void Scene::integrateAcceleration()
+{
+    //
+};
+
+void Scene::integrateVelocities()
+{
+    position += velocity * dt;
+    orientation += angularVelocity * dt;
+};
+
+void Scene::updatePosistions()
+{
+
+};
+
+void Scene::Step() 
+{
+
+}
+
+void Scene::draw()
+{
+
+};
